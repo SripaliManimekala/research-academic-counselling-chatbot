964cbe33a0ffff04f5d9623ee3db8c7047bca139
SripaliManimekala <sripalimanimekala@gmail.com>
2024-02-04 20:47:07 +0530

    bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -109,3 +109,25 @@ train(
     num_train_epochs=num_train_epochs,
     save_steps=save_steps
 )
+
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+def load_model(model_path):
+    model = GPT2LMHeadModel.from_pretrained(model_path)
+    return model
+
+def load_tokenizer(tokenizer_path):
+    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+    return tokenizer
+
+def generate_text(model, tokenizer, sequence, max_length):
+    ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
+    final_outputs = model.generate(
+        ids,
+        do_sample=True,
+        max_length=max_length,
+        pad_token_id=model.config.eos_token_id,
+        top_k=50,
+        top_p=0.95,
+    )
+    print(tokenizer.decode(final_outputs[0], skip_special_tokens=True))
